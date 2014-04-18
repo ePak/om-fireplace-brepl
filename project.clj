acede0655b5fecaa314bb3de6f27ea08f0fcc6fe
Eric Pak <erichyp@gmail.com>
2014-04-18 18:54:14 +1000

    Add :scm section to project.clj

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://github.com/ePak/om-fireplace-brepl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url "https://github.com/ePak/om-fireplace-brepl.git"}
   :eval-in-leiningen true)
